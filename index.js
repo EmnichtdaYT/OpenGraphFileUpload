13b0f2686950beb84d655bb66d210db76f3643e1
zoe <emnichtda@gmail.com>
2023-03-21 19:37:31 +0100

    initial login stuff owo

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const sqlite3 = require("sqlite3").verbose();
 const crypto = require("crypto");
 const multer = require("multer");
 const bodyParser = require("body-parser");
+const rateLimit = require("express-rate-limit");
 
 const db = new sqlite3.Database("credentials.db");
 
@@ -13,23 +14,49 @@ const port = process.env.PORT || 8080;
 const upload = multer();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const generalLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: "Too many requests, please try again later."
+});
+
+const loginLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 10, // limit each IP to 10 requests per windowMs
+  message: "Too many login attempts, please try again later."
+});
+
+
+app.use(generalLimiter);
+app.use("/login", loginLimiter);
+
+function hash(input){
+  return crypto.createHash("sha256").update(input).digest("hex");
+}
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "./view/index.html"));
 });
 
+app.get("/dashboard", function (req, res) {
+  res.sendFile(path.join(__dirname, "./view/dashboard.html"));
+});
+
 app.get('/login', (req, res) => {
     res.redirect(301, '/', 'Assuming you want to see the loin page. For authentication use POST on this endpoint.');
 });
-  
 
 app.post("/login", (req, res) => {
-  const { username, password } = req.body;
+  const { username, password, extendedExpire } = req.body;
 
-  console.log(username, password);
+  console.log(username, hash(password), extendedExpire);
 
   res.set('Content-Type', 'application/json');
-  res.status(401).send({message: "authentication failure"});
+  
+  res.status(200).send({token: "kiara", expiresIn: 999999});
+  //res.status(401).send({message: "authentication failure"});
 });
 
 app.post("/upload", upload.single("file"), (req, res) => {
@@ -57,8 +84,3 @@ db.all("SELECT * FROM users", (err, rows) => {
 
 // Close the database connection
 db.close();
-
-const text = "Hello, world!";
-const hash = crypto.createHash("sha256").update(text).digest("hex");
-
-console.log(hash);
