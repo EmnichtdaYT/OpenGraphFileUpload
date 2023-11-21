221edcac5a66f6455967be7d0b229a6050cb92a5
zoe <emnichtda@gmail.com>
2023-11-21 18:33:26 +0100

    lines with # now get ignored in .env

--- export_env_vars.sh
+++ export_env_vars.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Source the .env file and store the output in a variable
-ENV_VARS=$(cat .env | sed '/^$/d' | sed 's/^/export /')
+ENV_VARS=$(grep -v '^#' .env | sed '/^$/d' | sed 's/^/export /')
 
 # Export the environment variables
 eval "$ENV_VARS"
